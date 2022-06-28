a4b06fbdd2f93c3b03fff8bdcc6b34c916395e5c
Ollie <69084614+olijeffers0n@users.noreply.github.com>
2022-06-28 19:00:37 +0100

    Bump Version

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ with open("README.rst", "r") as f:
   push_receiver_readme = f.read()
 
 setup(
-    name="push_receiver",
-    version="0.4.0",
+    name="rustPlusPushReceiver",
+    version="0.4.1",
     author="Franc[e]sco",
     author_email="lolisamurai@tfwno.gf",
-    url="https://github.com/Francesco149/push_receiver",
+    url="https://github.com/olijeffers0n/push_receiver",
     packages=find_packages("."),
     description="subscribe to GCM/FCM and receive notifications",
     long_description=push_receiver_readme,
