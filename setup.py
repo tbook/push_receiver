a6aedbb51fedf7a4cdeb80755dc2b39bcbcd00c9
olijeffers0n <69084614+olijeffers0n@users.noreply.github.com>
2024-05-08 23:17:40 +0100

    Update to new Version?

--- setup.py
+++ setup.py
@@ -11,23 +11,21 @@ push_receiver_classifiers = [
 ]
 
 with open("README.rst", "r") as f:
-  push_receiver_readme = f.read()
+    push_receiver_readme = f.read()
 
 setup(
     name="rustPlusPushReceiver",
-    version="0.4.1",
-    author="Franc[e]sco",
-    author_email="lolisamurai@tfwno.gf",
+    version="0.5.0",
+    author="Franc[e]sco & olijeffers0n",
     url="https://github.com/olijeffers0n/push_receiver",
     packages=find_packages("."),
-    description="subscribe to GCM/FCM and receive notifications",
+    description="Subscribe to GCM/FCM and receive notifications",
     long_description=push_receiver_readme,
     license="Unlicense",
     classifiers=push_receiver_classifiers,
     keywords="fcm gcm push notification firebase google",
-    install_requires=["oscrypto", "protobuf"],
+    install_requires=["oscrypto", "protobuf", "http-ece", "cryptography", "betterproto", "requests"],
     extras_require={
-        "listen": ["http-ece"],
         "example": ["appdirs"]
     }
 )
